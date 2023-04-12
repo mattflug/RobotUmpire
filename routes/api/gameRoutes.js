0740c5646798e060edadaf7c6895b9969b47a027
Ghost <mbflug@gmail.com>
2023-04-12 02:05:41 -0600

    updating controllers

--- routes/api/gameRoutes.js
+++ routes/api/gameRoutes.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  updateUser,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUser).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/assignments/:assignmentId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
