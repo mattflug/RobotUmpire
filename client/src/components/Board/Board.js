687bd77e7cfe215b1e74c6393565a6e153d64a70
Ghost <mbflug@gmail.com>
2023-04-25 08:54:15 -0600

    added mui components for board

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -2,33 +2,24 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container disableGutters maxWidth={false}>
-        <Box>
-          <Box
-            sx={{
-              bgcolor: "#008080",
-              height: {
-                xs: "100vh",
-                sm: "130vh",
-                md: "140vh",
-                lg: "105vh",
-                xl: "150vh",
-              },
-              flexDirection: {
-                xs: "column",
-                sm: "row",
-                md: "row",
-                lg: "row",
-                xl: "row",
-              },
-              alignItems: { xs: "center" },
-            }}
-          ></Box>
-        </Box>
+      <Container maxWidth="false">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "primary.dark",
+            "&:hover": {
+              backgroundColor: "primary.main",
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        />
       </Container>
     </React.Fragment>
   );
