399636c2e8389ff4da934b00b81f027fb969604c
Ghost <mbflug@gmail.com>
2023-04-26 11:14:38 -0600

    centered container

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -9,17 +9,18 @@ export default function SimpleContainer() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="false">
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} >
-          <Box className= "border"
-          sx={{
-            width: 300,
-            height: 300,
-            backgroundColor: "primary.dark",
-            "&:hover": {
-              backgroundColor: "primary.main",
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
+        <Box className="container"  sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+          <Box
+            className="border"
+            sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
           />
         </Box>
       </Container>
