b95df8308642a34075983e6c2b0e0d9df5d89398
Ghost <mbflug@gmail.com>
2023-04-27 14:10:02 -0600

    added top and bottom containers on main

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -17,7 +17,30 @@ export default function SimpleContainer() {
               height: 600,
               backgroundColor: "#008080",
             }}
-          />
+          >
+            <Box
+              sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: "primary.dark",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+              }}
+            />
+            <Box
+              sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: "primary.dark",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                },
+              }}
+            />
+          </Box>
         </Box>
       </Container>
     </React.Fragment>
