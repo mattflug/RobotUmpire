8da747a99a50335dd2d21ca66955ab7b2567ae4a
Ghost <mbflug@gmail.com>
2023-05-01 17:21:28 -0600

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Board from "./components/Board/Board";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
     <>
-      <Router className= "router">
+      <Router >
         <Header />
         <Board />
+        <Footer />
       </Router>
     </>
   );
