22b99f68488c7fe52540a24cb13446d8be0d48a9
Ghost <mbflug@gmail.com>
2023-05-03 12:57:30 -0600

    added flex grow

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./Board.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
+
 import Container from "@mui/material/Container";
 
 export default function SimpleContainer() {
@@ -21,68 +21,14 @@ export default function SimpleContainer() {
           >
             <div className="main">
               <div className="home">
-                <Box
-                  sx={{
-                    display: "inline",
-                    flexWrap: "wrap",
-                    "& > :not(style)": {
-                      m: 1,
-                      width: 50,
-                      height: 50,
-                    },
-                  }}
-                >
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                </Box>
+                
               </div>
               <div className="away">
-                <Box
-                  sx={{
-                    display: "block",
-                    flexWrap: "wrap",
-                    flexDirection: "row",
-                    "& > :not(style)": {
-                      m: 1,
-                      width: 50,
-                      height: 50,
-                    },
-                  }}
-                >
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                  <Paper elevation={3} />
-                </Box>
+                
               </div>
             </div>
             <div className="sidebar">
-              <Box
-                sx={{
-                  display: "inline",
-                  flexWrap: "wrap",
-                  alignItems: "row",
-                  "& > :not(style)": {
-                    m: 1,
-                    width: 128,
-                    height: 128,
-                  },
-                }}
-              >
-                <Paper elevation={3} />
-                <Paper elevation={3} />
-                <Paper elevation={3} />
-              </Box>
+             
             </div>
           </Box>
         </Box>
