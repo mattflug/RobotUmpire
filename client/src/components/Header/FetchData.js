96ce2e8e3f1f602acf38398784b6a8aaad53420a
Ghost <mbflug@gmail.com>
2023-05-04 14:51:25 -0600

    brought mlb api to front end

--- client/src/components/Header/FetchData.js
+++ client/src/components/Header/FetchData.js
@@ -1,22 +1,25 @@
-import React from 'react'
-
+import React, { useState } from "react";
 
 function FetchData() {
+  const [data, setData] = useState([]);
 
+  const apiGet = () => {
+    fetch("http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+      });
+  };
 
-    const apiGet = () => {
-        fetch('http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1')
-            .then(response => response.json())
-            .then((json) => console.log(json))
-            .catch(err => console.log(err))
-    }
-
-    return (
-        <div>
-            My API <br />
-            <button onClick={apiGet}>Fetcch API</button>
-        </div>
-    );
+  return (
+    <div>
+      My API <br />
+      <button onClick={apiGet}>Fetch API</button>
+      <br />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
-    export default FetchData
+export default FetchData;
